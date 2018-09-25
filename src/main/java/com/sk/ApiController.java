7c57146790c146e231bfcde55b4dba23ac994196
lexsk <lexsdk@gmail.com>
2018-09-25 22:35:47 +0500

    Тестирование AJAX и вывода в React из PostgreSql

--- src/main/java/com/sk/ApiController.java
+++ src/main/java/com/sk/ApiController.java
@@ -1,18 +1,36 @@
 package com.sk;
 
-import org.springframework.stereotype.Controller;
+import com.sk.dao.CountryDao;
+import com.sk.entity.CountryEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 
 @RestController
 public class ApiController {
 
+    @Autowired
+    private CountryDao countryDao;
+
     @RequestMapping(value = "/get_token")
     public String getToken() throws UnsupportedEncodingException, NoSuchAlgorithmException {
         return Verifier.calcSHA1("123");
     }
 
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    @ResponseBody
+    public String sendMessage(HttpServletRequest request) {
+        String testId = request.getParameter("test_id");
+        int sum = 0;
+        for (CountryEntity entity : countryDao.getAll()) {
+            sum += entity.getId();
+        }
+        return String.valueOf(Integer.parseInt(testId) * 2) + " " + String.valueOf(sum);
+    }
 }
--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,9 +1,26 @@
 'use strict';
 
+import $ from "jquery";
+
 const React = require('react');
 const ReactDOM = require('react-dom');
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        $.ajax({
+            type: 'POST',
+            url: 'test',
+            data: 'test_id=123',
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    }
 
     render() {
         return (
